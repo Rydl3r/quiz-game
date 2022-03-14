5371f825200f5d32587411959071adf766f6b910
Ivan Mukoed <1rydler@gmail.com>
2022-03-14 18:36:19 +0200

    Small fixes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Question from './question';
 
 function App() {
 
-  
-  
+
+
   const [playing, setPlaying] = useState(false)
   const [showScore, setShowScore] = useState(false)
   const [score, setScore] = useState(0)
@@ -43,25 +43,25 @@ function App() {
 
 
 
-  if(playing === false) {
+  if (playing === false) {
     return (
-    <div className="App" style={{ backgroundImage: `url(${background})`,height: "100vh", backgroundSize: "cover"}}>
-          <h1>IT'S<br/>QUIZ<br/>TIME</h1>
-          <button href="something" className="button_hero" onClick={startGame}>Play</button>
-    </div>
+      <div className="App" style={{ backgroundImage: `url(${background})`, height: "100vh", backgroundSize: "cover", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+        <h1>IT'S<br />QUIZ<br />TIME</h1>
+        <button href="something" className="button_hero" onClick={startGame}>Play</button>
+      </div>
     );
-  } else if(showFinal === true) {
-    return(
-      <div className="App" style={{ backgroundImage: `url(${background})`,height: "100vh", backgroundSize: "cover"}}>
-          <h1>That's<br/>it!</h1>
-          <h3>Your final score - {score}</h3>
-          <button href="something" className="button_hero" onClick={restartGame}>Restart the game</button>
-    </div>
+  } else if (showFinal === true) {
+    return (
+      <div className="App" style={{ backgroundImage: `url(${background})`, height: "100vh", backgroundSize: "cover", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+        <h1>That's<br />it!</h1>
+        <h3>Your final score - {score}</h3>
+        <button href="something" className="button_hero" onClick={restartGame}>Restart the game</button>
+      </div>
     )
   } else {
     return (
-      <div className="App" style={{ backgroundImage: `url(${background})`,height: "100vh", backgroundSize: "cover"}}>
-        <Question score={score} showScore={showScore} setShowScore={setShowScore} question={questions[index]} setScore={setScore} setIndex={setIndex} index={index} resultInfo={resultInfo} setResultInfo={setResultInfo} playing={playing} setPlaying={setPlaying} showFinal={showFinal} setShowFinal={setShowFinal} showButtons={showButtons} setShowButtons={setShowButtons}/>
+      <div className="App" style={{ backgroundImage: `url(${background})`, height: "100vh", backgroundSize: "cover", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Question score={score} showScore={showScore} setShowScore={setShowScore} question={questions[index]} setScore={setScore} setIndex={setIndex} index={index} resultInfo={resultInfo} setResultInfo={setResultInfo} playing={playing} setPlaying={setPlaying} showFinal={showFinal} setShowFinal={setShowFinal} showButtons={showButtons} setShowButtons={setShowButtons} />
       </div>
     )
   }
