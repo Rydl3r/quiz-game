d93b3461b72eb5522fd4fb91739b7d841a5161fe
Ivan Mukoed <1rydler@gmail.com>
2022-06-14 17:31:45 +0300

    syntax fixes

--- src/App.js
+++ src/App.js
@@ -1,69 +1,125 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import background from "./img/bg.png";
-import Question from './question';
+import Question from "./question";
 
 function App() {
-
-
-
-  const [playing, setPlaying] = useState(false)
-  const [showScore, setShowScore] = useState(false)
-  const [score, setScore] = useState(0)
-  const [questions, setQuestions] = useState([])
-  const [index, setIndex] = useState(0)
-  const [resultInfo, setResultInfo] = useState(`Right! Your Score - ${score}`)
-  const [showFinal, setShowFinal] = useState(false)
-  const [showButtons, setShowButtons] = useState(true)
+  const [playing, setPlaying] = useState(false);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [resultInfo, setResultInfo] = useState(`Right! Your Score - ${score}`);
+  const [showFinal, setShowFinal] = useState(false);
+  const [showButtons, setShowButtons] = useState(true);
 
   const startGame = () => {
-    setPlaying(true)
-    setIndex(0)
-  }
+    setPlaying(true);
+    setIndex(0);
+  };
 
   const fetchQuestions = async () => {
-    let response = await fetch('https://opentdb.com/api.php?amount=10')
-    let data = await response.json()
-    setQuestions(data.results)
-  }
+    let response = await fetch("https://opentdb.com/api.php?amount=10");
+    let data = await response.json();
+    setQuestions(data.results);
+  };
 
   const restartGame = () => {
-    setIndex(0)
-    setPlaying(false)
-    setShowScore(false)
-    setShowFinal(false)
-    setScore(0)
-    setShowButtons(true)
-    fetchQuestions()
-  }
+    setIndex(0);
+    setPlaying(false);
+    setShowScore(false);
+    setShowFinal(false);
+    setScore(0);
+    setShowButtons(true);
+    fetchQuestions();
+  };
 
   useEffect(() => {
     fetchQuestions();
-  }, [])
-
-
-
+  }, []);
 
   if (playing === false) {
     return (
-      <div className="App" style={{ backgroundImage: `url(${background})`, height: "100vh", backgroundSize: "cover", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-        <h1>IT'S<br />QUIZ<br />TIME</h1>
-        <button href="something" className="button_hero" onClick={startGame}>Play</button>
+      <div
+        className="App"
+        style={{
+          backgroundImage: `url(${background})`,
+          height: "100vh",
+          backgroundSize: "cover",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>
+          IT'S
+          <br />
+          QUIZ
+          <br />
+          TIME
+        </h1>
+        <button href="something" className="button_hero" onClick={startGame}>
+          Play
+        </button>
       </div>
     );
   } else if (showFinal === true) {
     return (
-      <div className="App" style={{ backgroundImage: `url(${background})`, height: "100vh", backgroundSize: "cover", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-        <h1>That's<br />it!</h1>
+      <div
+        className="App"
+        style={{
+          backgroundImage: `url(${background})`,
+          height: "100vh",
+          backgroundSize: "cover",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>
+          That's
+          <br />
+          it!
+        </h1>
         <h3>Your final score - {score}</h3>
-        <button href="something" className="button_hero" onClick={restartGame}>Restart the game</button>
+        <button href="something" className="button_hero" onClick={restartGame}>
+          Restart the game
+        </button>
       </div>
-    )
+    );
   } else {
     return (
-      <div className="App" style={{ backgroundImage: `url(${background})`, height: "100vh", backgroundSize: "cover", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <Question score={score} showScore={showScore} setShowScore={setShowScore} question={questions[index]} setScore={setScore} setIndex={setIndex} index={index} resultInfo={resultInfo} setResultInfo={setResultInfo} playing={playing} setPlaying={setPlaying} showFinal={showFinal} setShowFinal={setShowFinal} showButtons={showButtons} setShowButtons={setShowButtons} />
+      <div
+        className="App"
+        style={{
+          backgroundImage: `url(${background})`,
+          height: "100vh",
+          backgroundSize: "cover",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Question
+          score={score}
+          showScore={showScore}
+          setShowScore={setShowScore}
+          question={questions[index]}
+          setScore={setScore}
+          setIndex={setIndex}
+          index={index}
+          resultInfo={resultInfo}
+          setResultInfo={setResultInfo}
+          playing={playing}
+          setPlaying={setPlaying}
+          showFinal={showFinal}
+          setShowFinal={setShowFinal}
+          showButtons={showButtons}
+          setShowButtons={setShowButtons}
+        />
       </div>
-    )
+    );
   }
 }
 
--- src/question.js
+++ src/question.js
@@ -1,105 +1,207 @@
-import React from 'react'
+import React from "react";
 
-const question = ({showButtons, setShowButtons, score , showScore, setShowScore, question, setScore, setIndex, index, resultInfo, setResultInfo, playing, setPlaying, showFinal, setShowFinal}) => {
-    
-    function htmlDecode(input){
-        var e = document.createElement('textarea');
-        e.innerHTML = input;
-        // handle case of empty input
-        return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
-      }
+const question = ({
+  showButtons,
+  setShowButtons,
+  score,
+  showScore,
+  setShowScore,
+  question,
+  setScore,
+  setIndex,
+  index,
+  resultInfo,
+  setResultInfo,
+  playing,
+  setPlaying,
+  showFinal,
+  setShowFinal,
+}) => {
+  function htmlDecode(input) {
+    var e = document.createElement("textarea");
+    e.innerHTML = input;
+    // handle case of empty input
+    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+  }
 
-    const correct_answer = () => {
-        setResultInfo(`Right! Your Score - ${score}`)
-        let newScore = score+1
-        let newIndex = index+1;
-        setScore(newScore)
-        setShowButtons(false)
-        setShowScore(true)
-        if(newIndex > 9) {
-            setShowFinal(true)
-        } else {
-            setTimeout(() => {
-                setIndex(newIndex)
-                setShowButtons(true)
-                setShowScore(false)
-            }, 1000);
-
-        }
-
-        
-        
+  const correct_answer = () => {
+    setResultInfo(`Right! Your Score - ${score}`);
+    let newScore = score + 1;
+    let newIndex = index + 1;
+    setScore(newScore);
+    setShowButtons(false);
+    setShowScore(true);
+    if (newIndex > 9) {
+      setShowFinal(true);
+    } else {
+      setTimeout(() => {
+        setIndex(newIndex);
+        setShowButtons(true);
+        setShowScore(false);
+      }, 1000);
     }
+  };
 
-    const incorrect_answer = () => {
-        setResultInfo(`Wrong! Your Score - ${score}`)
-        let newScore = score;
-        let newIndex = index+1;
-        setScore(newScore)
-        setShowButtons(false)
-        setShowScore(true)
-        if(newIndex > 9) {
-            setShowFinal(true)
-        } else {
-            setTimeout(() => {
-                setIndex(newIndex)
-                setShowButtons(true)
-                setShowScore(false)
-            }, 1000);
-        }
+  const incorrect_answer = () => {
+    setResultInfo(`Wrong! Your Score - ${score}`);
+    let newScore = score;
+    let newIndex = index + 1;
+    setScore(newScore);
+    setShowButtons(false);
+    setShowScore(true);
+    if (newIndex > 9) {
+      setShowFinal(true);
+    } else {
+      setTimeout(() => {
+        setIndex(newIndex);
+        setShowButtons(true);
+        setShowScore(false);
+      }, 1000);
     }
+  };
 
-    let randomNumber = Math.floor(Math.random() * 4) + 1;
+  let randomNumber = Math.floor(Math.random() * 4) + 1;
 
-    let orderOfIncorrect = [1,2,3,3]
+  let orderOfIncorrect = [1, 2, 3, 3];
 
-    if(randomNumber === 1) {
-        orderOfIncorrect.length = 0;                  // Clear contents
-        orderOfIncorrect.push.apply(orderOfIncorrect, [0,0,1,2]);
-    } else if(randomNumber === 2) {
-        orderOfIncorrect.length = 0;                  // Clear contents
-        orderOfIncorrect.push.apply(orderOfIncorrect, [0,0,1,2]);
-    } else if(randomNumber === 3) {
-        orderOfIncorrect.length = 0;                  // Clear contents
-        orderOfIncorrect.push.apply(orderOfIncorrect, [0,1,1,2]);
-    } else if(randomNumber === 4) {
-        orderOfIncorrect.length = 0;                  // Clear contents
-        orderOfIncorrect.push.apply(orderOfIncorrect, [0,1,2,2]);
-    }
+  if (randomNumber === 1) {
+    orderOfIncorrect.length = 0; // Clear contents
+    orderOfIncorrect.push.apply(orderOfIncorrect, [0, 0, 1, 2]);
+  } else if (randomNumber === 2) {
+    orderOfIncorrect.length = 0; // Clear contents
+    orderOfIncorrect.push.apply(orderOfIncorrect, [0, 0, 1, 2]);
+  } else if (randomNumber === 3) {
+    orderOfIncorrect.length = 0; // Clear contents
+    orderOfIncorrect.push.apply(orderOfIncorrect, [0, 1, 1, 2]);
+  } else if (randomNumber === 4) {
+    orderOfIncorrect.length = 0; // Clear contents
+    orderOfIncorrect.push.apply(orderOfIncorrect, [0, 1, 2, 2]);
+  }
 
-    const returnButtons = () => {
-        return (
-            "cokc"
-        )
-    }
+  const returnButtons = () => {
+    return "cokc";
+  };
 
-    return (
+  return (
+    <div>
+      <h2 className="question_title">{htmlDecode(question.question)}</h2>
+      {showButtons === true && showScore === false ? (
         <div>
-            <h2 className="question_title">{htmlDecode(question.question)}</h2>
-            {showButtons=== true&& showScore===false ? 
-            <div>{question.incorrect_answers.length === 1 ? 
-                <div className="buttons_wrapper">    
-                    {randomNumber===1||randomNumber===2 ? <button href="something" className="button_hero" onClick={correct_answer}>{htmlDecode(question.correct_answer)}</button>:<button href="something" className="button_hero" onClick={incorrect_answer}>{htmlDecode(question.incorrect_answers[0])}</button>}
-                    {randomNumber===3||randomNumber===4 ? <button href="something" className="button_hero" onClick={correct_answer}>{htmlDecode(question.correct_answer)}</button>:<button href="something" className="button_hero" onClick={incorrect_answer}>{htmlDecode(question.incorrect_answers[0])}</button>}
-                </div> 
-                : 
-                <div className="buttons_wrapper">
-                    {randomNumber===1 ? <button href="something" className="button_hero" onClick={correct_answer}>{htmlDecode(question.correct_answer)}</button>:<button href="something" className="button_hero" onClick={incorrect_answer}>{htmlDecode(question.incorrect_answers[orderOfIncorrect[0]])}</button>}
-                    {randomNumber===2 ? <button href="something" className="button_hero" onClick={correct_answer}>{htmlDecode(question.correct_answer)}</button>:<button href="something" className="button_hero" onClick={incorrect_answer}>{htmlDecode(question.incorrect_answers[orderOfIncorrect[1]])}</button>}
-                    {randomNumber===3 ? <button href="something" className="button_hero" onClick={correct_answer}>{htmlDecode(question.correct_answer)}</button>:<button href="something" className="button_hero" onClick={incorrect_answer}>{htmlDecode(question.incorrect_answers[orderOfIncorrect[2]])}</button>}
-                    {randomNumber===4 ? <button href="something" className="button_hero" onClick={correct_answer}>{htmlDecode(question.correct_answer)}</button>:<button href="something" className="button_hero" onClick={incorrect_answer}>{htmlDecode(question.incorrect_answers[orderOfIncorrect[3]])}</button>}
-                </div>
-                }</div> 
-            : 
-            <div>{showScore ? <h3>{resultInfo}</h3> : <h3></h3>}</div>}
-
-
-
-            
-
-            
+          {question.incorrect_answers.length === 1 ? (
+            <div className="buttons_wrapper">
+              {randomNumber === 1 || randomNumber === 2 ? (
+                <button
+                  href="something"
+                  className="button_hero"
+                  onClick={correct_answer}
+                >
+                  {htmlDecode(question.correct_answer)}
+                </button>
+              ) : (
+                <button
+                  href="something"
+                  className="button_hero"
+                  onClick={incorrect_answer}
+                >
+                  {htmlDecode(question.incorrect_answers[0])}
+                </button>
+              )}
+              {randomNumber === 3 || randomNumber === 4 ? (
+                <button
+                  href="something"
+                  className="button_hero"
+                  onClick={correct_answer}
+                >
+                  {htmlDecode(question.correct_answer)}
+                </button>
+              ) : (
+                <button
+                  href="something"
+                  className="button_hero"
+                  onClick={incorrect_answer}
+                >
+                  {htmlDecode(question.incorrect_answers[0])}
+                </button>
+              )}
+            </div>
+          ) : (
+            <div className="buttons_wrapper">
+              {randomNumber === 1 ? (
+                <button
+                  href="something"
+                  className="button_hero"
+                  onClick={correct_answer}
+                >
+                  {htmlDecode(question.correct_answer)}
+                </button>
+              ) : (
+                <button
+                  href="something"
+                  className="button_hero"
+                  onClick={incorrect_answer}
+                >
+                  {htmlDecode(question.incorrect_answers[orderOfIncorrect[0]])}
+                </button>
+              )}
+              {randomNumber === 2 ? (
+                <button
+                  href="something"
+                  className="button_hero"
+                  onClick={correct_answer}
+                >
+                  {htmlDecode(question.correct_answer)}
+                </button>
+              ) : (
+                <button
+                  href="something"
+                  className="button_hero"
+                  onClick={incorrect_answer}
+                >
+                  {htmlDecode(question.incorrect_answers[orderOfIncorrect[1]])}
+                </button>
+              )}
+              {randomNumber === 3 ? (
+                <button
+                  href="something"
+                  className="button_hero"
+                  onClick={correct_answer}
+                >
+                  {htmlDecode(question.correct_answer)}
+                </button>
+              ) : (
+                <button
+                  href="something"
+                  className="button_hero"
+                  onClick={incorrect_answer}
+                >
+                  {htmlDecode(question.incorrect_answers[orderOfIncorrect[2]])}
+                </button>
+              )}
+              {randomNumber === 4 ? (
+                <button
+                  href="something"
+                  className="button_hero"
+                  onClick={correct_answer}
+                >
+                  {htmlDecode(question.correct_answer)}
+                </button>
+              ) : (
+                <button
+                  href="something"
+                  className="button_hero"
+                  onClick={incorrect_answer}
+                >
+                  {htmlDecode(question.incorrect_answers[orderOfIncorrect[3]])}
+                </button>
+              )}
+            </div>
+          )}
         </div>
-    )
-}
+      ) : (
+        <div>{showScore ? <h3>{resultInfo}</h3> : <h3></h3>}</div>
+      )}
+    </div>
+  );
+};
 
-export default question
+export default question;
